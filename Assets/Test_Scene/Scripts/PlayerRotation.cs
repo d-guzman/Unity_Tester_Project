3a2fc481965e7b24a841b032ae925a300077aab6
d-guzman <dghost121@gmail.com>
2016-11-04 22:24:47 -0700

    Movement System: 4-Player Party Minigame
    Implemented control system to allow for 4 players to move separately on
    a game board. Rather basic. Allows room for expansion.

---
+++ Assets/Test_Scene/Scripts/PlayerRotation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRotation : MonoBehaviour {
+    public GameObject player;
+
+    void Update() {
+        if (player.name == "P1_Model") {
+            var VectorManager = player.GetComponentInParent<PlayerTranslations>();
+            if (VectorManager.getPlayer1Move() != Vector3.zero) {
+                Quaternion player1_Rotate = Quaternion.LookRotation(VectorManager.getPlayer1Move());
+                player.transform.rotation = Quaternion.Slerp(player.transform.rotation, player1_Rotate, 10.0f * Time.deltaTime);
+            }
+        }
+        if (player.name == "P2_Model")
+        {
+            var VectorManager = player.GetComponentInParent<PlayerTranslations>();
+            if (VectorManager.getPlayer2Move() != Vector3.zero)
+            {
+                Quaternion player2_Rotate = Quaternion.LookRotation(VectorManager.getPlayer2Move());
+                player.transform.rotation = Quaternion.Slerp(player.transform.rotation, player2_Rotate, 10.0f * Time.deltaTime);
+            }
+        }
+        if (player.name == "P3_Model")
+        {
+            var VectorManager = player.GetComponentInParent<PlayerTranslations>();
+            if (VectorManager.getPlayer3Move() != Vector3.zero)
+            {
+                Quaternion player3_Rotate = Quaternion.LookRotation(VectorManager.getPlayer3Move());
+                player.transform.rotation = Quaternion.Slerp(player.transform.rotation, player3_Rotate, 10.0f * Time.deltaTime);
+            }
+        }
+        if (player.name == "P4_Model")
+        {
+            var VectorManager = player.GetComponentInParent<PlayerTranslations>();
+            if (VectorManager.getPlayer4Move() != Vector3.zero)
+            {
+                Quaternion player4_Rotate = Quaternion.LookRotation(VectorManager.getPlayer4Move());
+                player.transform.rotation = Quaternion.Slerp(player.transform.rotation, player4_Rotate, 10.0f * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Test_Scene/Scripts/PlayerTranlation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTranlation : MonoBehaviour {
+    //Gets the player that the script is connected to.
+    public GameObject player;
+
+    // Update is called once per frame
+	void Update () {
+        if (player.name == "Player1") {
+            var vectorManager = player.GetComponentInParent<PlayerTranslations>();
+            player.transform.Translate(vectorManager.getPlayer1Move());
+        }
+
+        if (player.name == "Player2") {
+            var vectorManager = player.GetComponentInParent<PlayerTranslations>();
+            player.transform.Translate(vectorManager.getPlayer2Move());
+        }
+
+        if (player.name == "Player3")
+        {
+            var vectorManager = player.GetComponentInParent<PlayerTranslations>();
+            player.transform.Translate(vectorManager.getPlayer3Move());
+        }
+
+        if (player.name == "Player4")
+        {
+            var vectorManager = player.GetComponentInParent<PlayerTranslations>();
+            player.transform.Translate(vectorManager.getPlayer4Move());
+        }
+    }
+}
