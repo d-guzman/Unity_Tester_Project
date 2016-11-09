c6400d0d79a8b8358fdd796495c6ad607ab033e8
d-guzman <dghost121@gmail.com>
2016-11-09 14:07:11 -0800

    Refinement of Movement
    Changelist
    - Made it so that players don't need to be in a parent object to move.
    - Made player prefabs.
    - If a player doesn't have a specific name, Unity will raise an
    exception about how there isn't a <playerName>_<InputName> component in
    the InputManager.

---
+++ Assets/Character_Controller_Test/Scripts/Get_Players_Active.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Get_Players_Active : MonoBehaviour {
+    GameObject[] players;
+    // Update is called once per frame
+    void Start() {
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+}
